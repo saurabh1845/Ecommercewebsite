33db2ecd1cb52effffa3c44a5feef9d2366f79a2
saurabh1845 <saurabhsharma4518@gmail.com>
2024-01-28 11:17:35 +0530

    Pushing code

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,18 @@
+export const mainCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
+        path:""
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        path:""
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+        path:""
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
+        path:""
+    }
+]
